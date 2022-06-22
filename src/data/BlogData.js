e693a33b9ad4a53d0986eb53ed986e83b633266a
Harsh Kanodiya <72756692+Harsh3363@users.noreply.github.com>
2022-06-22 19:30:31 +0530

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,11 +1,11 @@
 export const Blogs = [
     {
         id:1,
-        name:"Under Construction",
+        name:"Solidity – Programming ERC-721 Token",
         tags:[""],
         date:"",
-        imgSrc:"https://media.istockphoto.com/photos/webpage-under-construction-picture-id592021466",
-        link:""
+        imgSrc:"https://pbs.twimg.com/profile_images/1476085383469445124/1ZRVN-QB_400x400.jpg",
+        link:"https://www.geeksforgeeks.org/solidity-programming-erc-721-token/"
     },
     {
         id:2,
