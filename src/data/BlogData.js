88e65e75a12d97224782a79543aa655a093d3392
Harsh Kanodiya <72756692+Harsh3363@users.noreply.github.com>
2023-11-14 18:59:35 +0530

    Update BlogData.js
    Added some articles links and made logo changes

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -4,32 +4,32 @@ export const Blogs = [
         name:"Solidity – Programming ERC-721 Token",
         tags:["Blockchain #Ethereum"],
         date:"",
-        imgSrc:"https://pbs.twimg.com/profile_images/1476085383469445124/1ZRVN-QB_400x400.jpg",
+        imgSrc:"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png",
         link:"https://www.geeksforgeeks.org/solidity-programming-erc-721-token/"
     },
     {
         id:2,
-        name:"Difference Between Ethereum and Solana",
-        tags:["Blockchain #Ethereum #Solana"],
-        date:"",
-        imgSrc:"https://pbs.twimg.com/profile_images/1476085383469445124/1ZRVN-QB_400x400.jpg",
-        link:"https://www.geeksforgeeks.org/difference-between-ethereum-and-solana/"
+        name:"Using MedusaJS with MongoDB",
+        tags:["Medusa #MongoDB"],
+        date:"May 2",
+        imgSrc:"https://miro.medium.com/v2/resize:fit:1200/1*jfdwtvU6V6g99q3G7gq7dQ.png",
+        link:"https://medium.com/@harsh.kanodiya/using-medusajs-with-mongodb-7c3c896756db"
     },
     {
         id:3,
-        name:"Introduction to Polygon",
-        tags:["Blockchain #Polygon"],
+        name:"Difference Between Ethereum and Solana",
+        tags:["Blockchain #Ethereum #Solana"],
         date:"",
-        imgSrc:"https://pbs.twimg.com/profile_images/1476085383469445124/1ZRVN-QB_400x400.jpg",
-        link:"https://www.geeksforgeeks.org/introduction-to-polygon/"
+        imgSrc:"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png",
+        link:"https://www.geeksforgeeks.org/difference-between-ethereum-and-solana/"
     },
     {
         id:4,
-        name:"Under Construction",
-        tags:[""],
+        name:"Introduction to Polygon",
+        tags:["Blockchain #Polygon"],
         date:"",
-        imgSrc:"https://media.istockphoto.com/photos/webpage-under-construction-picture-id592021466",
-        link:""
+        imgSrc:"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png",
+        link:"https://www.geeksforgeeks.org/introduction-to-polygon/"
     },
     {
         id:5,
